9801f8ab364f15f46bd4d005298a2d96bedc8fc1
Tomáš Labanc <166916054+Tomaxikz@users.noreply.github.com>
2026-07-11 00:53:54 +0200

    Fixed lint

--- src/bins.rs
+++ src/bins.rs
@@ -332,7 +332,7 @@ mod tests {
 
         assert_eq!(
             path.file_name().and_then(|name| name.to_str()),
-            Some("dbev-socket-bridge-1")
+            Some("dbev-socket-bridge-2")
         );
         assert_eq!(
             fs::metadata(&path).unwrap().permissions().mode() & 0o777,
